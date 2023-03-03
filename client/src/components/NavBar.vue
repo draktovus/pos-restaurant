8a6f3fcfe2db1e0942881539ab48f2a0974196d1
Bryan De Anda <bryandeanda2012@gmail.com>
2023-03-03 13:18:58 -0500

    added components folder and started navbar

---
+++ client/src/components/NavBar.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+
+</script>
+<template>
+    <nav class="navbar is-dark" role="navigation" aria-label="main navigation">
+        <div class="navbar-menu">
+            <div class="navbar-start">
+                <div class="navbar-item">
+                    <div class="buttons">
+                        <a class="button is-light">
+                            <strong>Login</strong>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="tabs mb-0 is-toggle">
+                <ul>
+                    <li class="is-active"><a>Breakfast</a></li>
+                    <li><a>Lunch</a></li>
+                    <li><a>Dinner</a></li>
+                    <li><a>Sides</a></li>
+                    <li><a>Drinks</a></li>
+                </ul>
+            </div>
+            <div class="navbar-end">
+                <div class="navbar-item">
+                    <div class="buttons">
+                        <a class="button is-light">
+                            <strong>Admin</strong>
+                        </a>
+                        <a class="button is-danger">
+                            <strong>Checkout</strong>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav>
+</template>
+
+
+
+<style scoped>
+
+</style>
