80d6c0cef226b901814addd85a83d93a56429d0d
jackgusler <guslerj1@newpaltz.edu>
2023-03-04 11:14:55 -0500

    Started login page and added to components folder

---
+++ client/src/components/Login.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+</script>
+<template>
+<h2>Login</h2>
+<form>
+    <div>
+      <label for="uname"><b>Username</b></label>
+      <input type="text" placeholder="Enter Username" name="uname" required>
+      <br>
+      <label for="psw"><b>Password</b></label>
+      <input type="password" placeholder="Enter Password" name="psw" required>
+          
+      <button type="submit">Login</button>
+      <label>
+        <br>
+        <input type="checkbox" checked="checked" name="remember"> Remember me
+      </label>
+    </div>
+    <div>
+      <span class="psw">Forgot <a href="#">password?</a></span>
+    </div>
+</form>
+</template>
+<style scoped>
+</style>
