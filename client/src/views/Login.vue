6ba294021abc782a56e8ed716cda077f98262ba1
Bryan De Anda <bryandeanda2012@gmail.com>
2023-03-24 16:32:40 -0400

    started admin page with temp user data file

---
+++ client/src/views/Login.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div>
+        <h1 class="title">
+            Login
+        </h1>
+        <h2 class="subtitle">
+            Please login to create orders
+        </h2>
+    </div>
+</template>
+
+
+
+<style scoped>
+
+</style>
