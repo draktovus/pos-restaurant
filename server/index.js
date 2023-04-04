0dc3bec1864d769f84f1614fd781212ba42762bb
jackgusler <guslerj1@newpaltz.edu>
2023-04-04 16:07:02 -0400

    Added database

--- server/index.js
+++ server/index.js
@@ -1,9 +1,20 @@
+require('dotenv').config();
+
 const http = require('http');
-const express =  require('express');
+const routes = require('../routes/routes');
+const express = require('express');
+const mongoose = require('mongoose');
+const mongoString = process.env.DATABASE_URL;
 const app = express();
+
+app.use(express.json());
+app.use('/api', routes);
 
 const hostname = '127.0.0.1';
 const port = process.env.PORT || 3000;
+
+mongoose.connect(mongoString);
+const database = mongoose.connection
 
 app.get('/', (req, res) => {
     res.send("Server was accessed")
@@ -11,4 +22,16 @@ app.get('/', (req, res) => {
 
 app.listen(port, ()=>{
     console.log(`Listening at http://${hostname}:${port}`);
+})
+
+app.listen(3001, () => {
+    console.log(`Server Started at ${3001}`)
+})
+
+database.on('error', (error) => {
+    console.log(error)
+})
+
+database.once('connected', () => {
+    console.log('Database Connected');
 })
