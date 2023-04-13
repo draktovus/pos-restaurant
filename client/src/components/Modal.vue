45b60fdc24d7b3b1e98a9c37ad0898ecbb5398b3
Viri Marin <marinhev1@newpaltz.edu>
2023-04-13 17:12:25 -0400

    created general modal

---
+++ client/src/components/Modal.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+    const props = defineProps<{
+        title?:string;
+        isOpen: boolean;
+    }>();
+
+    const emit = defineEmits<{
+        (e: 'update:isOpen', value: boolean): void;
+    }>();
+</script>
+<template>
+    <div class="modal" :class="{ 'is-active' : props.isOpen }">
+        <div class="modal-background"></div>
+
+        <div class="moddal-card">
+            <header class="modal-card-head">
+                <slot class="header">
+                    <p class="modal-card-title">{{ props.title }}</p>
+                    <button class="delete" area-label="close" @click="emit('update:isOpen', false)"></button>
+                </slot>
+            </header>
+            <section class="modal-card-body">
+               <slot></slot>
+            </section>
+            <footer class="modal-card-foot">
+                <slot class="footer">
+                    <botton class="button is-success">Save changes</botton>
+                    <button class="button">
+                        Cancel
+                    </button>
+                </slot>
+            </footer>
+        </div>
+    </div>
+</template>
+
+
+<style scoped>
+
+</style>
